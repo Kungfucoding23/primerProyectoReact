a0ae0821c953d3a4a97bd0bb730ab55cca989e90
Alejandro <alesantin012@hotmail.com>
2020-07-25 09:25:58 -0300

    Prestamos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Header from './componentes/Header';
 import Formulario from './componentes/Formulario';
 
 function App() {
-    return ( <
-        Fragment >
-        <
-        Header titulo = "Cotizador de prestamos" /
-        >
-        <
-        div className = "container" >
-        <
-        Formulario / >
-        <
-        /div> < /
-        Fragment >
+   //Definir el state
+   const [cantidad, guardarCantidad] = useState(0);
+
+    return ( 
+        <Fragment>
+        <Header titulo = "Cotizador de prestamos"
+        />
+        <div className = "container">
+        <Formulario
+            cantidad={cantidad}
+            guardarCantidad={guardarCantidad}
+        />
+        </div> 
+        </Fragment>
     );
 }
 
