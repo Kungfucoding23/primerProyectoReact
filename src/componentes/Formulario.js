8129f9f6bc23f02adea83cdcd73c15443bb96d71
Alejandro <alesantin012@hotmail.com>
2020-07-25 11:35:22 -0300

    fin

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -4,7 +4,7 @@ import {calcularTotal} from '../helpers';
 
 const Formulario = (props) => {
 
-    const {cantidad, guardarCantidad, plazo, guardarPlazo, total, guardarTotal, guardarCargando} = props;
+    const {cantidad, guardarCantidad, plazo, guardarPlazo, guardarTotal, guardarCargando} = props;
 
     //Definir state
     const [error, guardarError] = useState(false);
